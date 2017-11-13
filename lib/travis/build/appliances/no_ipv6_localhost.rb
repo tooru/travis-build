7bd21804d1f24f74482cd90229f4d05611f7f227
Hiro Asari <asari.ruby@gmail.com>
2017-11-13 16:11:02 -0500

    Split IPv6 localhost disabling code into 2 parts
    So that it can run on containers

--- lib/travis/build/appliances/no_ipv6_localhost.rb
+++ lib/travis/build/appliances/no_ipv6_localhost.rb
@@ -5,7 +5,8 @@ module Travis
     module Appliances
       class NoIpv6Localhost < Base
         def apply
-          sh.raw %(sudo sed -e 's/^\\([0-9a-f:]\\+\\t\\)localhost/\\1/' -i'.bak' /etc/hosts)
+          sh.raw %(sed -e 's/^\\([0-9a-f:]\\+[ \\t]\\+\\)localhost/\\1/' /etc/hosts > /tmp/hosts.ipv6)
+          sh.raw %(sudo mv /tmp/hosts.ipv6 /etc/hosts)
         end
       end
     end
